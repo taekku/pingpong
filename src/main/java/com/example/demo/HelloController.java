ed2e177c9bc1e2f4f4805bad795f0a33b90707ff
Taekku Lim <taekku.lim@gmail.com>
2018-11-13 14:48:56 +0900

    SpringBoot

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Oh! My God! Greetings from Spring Boot!";
+    }
+    @GetMapping("/pingpong")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
