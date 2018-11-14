debce43a8292383c124a208505a26d3598dfd96f
T.G. Lim <taekgu@gmail.com>
2018-11-14 22:38:23 +0900

    Do Not Work

--- src/main/java/pingpong/web/controller/HelloController.java
+++ src/main/java/pingpong/web/controller/HelloController.java
@@ -1,6 +1,7 @@
 package pingpong.web.controller;
 
 
+import java.util.Date;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     
-    // @GetMapping("/")
-    // public String index(Map<String, Object> model) {
-    //     model.put("time", new Date());
-    //     model.put("message", "asdf My Message asdf");
-    //     return "index";
-    // }
+    @GetMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "asdf My Message asdf");
+        return "index";
+    }
     // @RequestMapping(value="/ping", method = RequestMethod.GET)
     // public ModelAndView hello(
     //                     // @RequestParam("sayit")
--- src/test/java/pingpong/main/DemoApplicationTests.java
+++ src/test/java/pingpong/main/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package pingpong.main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
