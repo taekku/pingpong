2b345ee38cf1d184597a5559de0e00dccd7c33f9
Taekku Lim <taekku.lim@gmail.com>
2018-11-15 16:54:27 +0900

    Good Jobs

--- src/main/java/pingpong/web/controller/HelloController.java
+++ src/main/java/pingpong/web/controller/HelloController.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -16,16 +19,16 @@ public class HelloController {
         model.put("message", "asdf My Message asdf");
         return "index";
     }
-    // @RequestMapping(value="/ping", method = RequestMethod.GET)
-    // public ModelAndView hello(
-    //                     // @RequestParam("sayit")
-    //                     // String sayit
-    //                     ) {
-		// ModelAndView retVal = new ModelAndView();
-		// retVal.setViewName("hello");
-		// retVal.addObject("message", "Hello my boys!");
-    // return retVal;
-    // }
+    @RequestMapping(value="/ping", method = RequestMethod.GET)
+    public ModelAndView hello(
+                        // @RequestParam("sayit")
+                        // String sayit
+                        ) {
+		ModelAndView retVal = new ModelAndView();
+		retVal.setViewName("ping");
+		retVal.addObject("message", "Hello my boys!");
+        return retVal;
+    }
     @GetMapping("/hello")
     public String ping(Map<String, Object> model) {
         // model.put("time", new Date());
