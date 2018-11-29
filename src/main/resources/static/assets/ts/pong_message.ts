c656de7129a61c79a47de85fc7ca464024c6fa38
T.G. Lim <taekgu@gmail.com>
2018-11-29 19:11:20 +0900

    my mesage

---
+++ src/main/resources/static/assets/ts/pong_message.ts
@@ -0,0 +1,11 @@
+class PongMessage{
+  public name: string;
+  private _age: number;
+  private kkk: number;
+  constructor(){
+    this._age = 30;
+    this.name = "Taekku";
+  }
+}
+
+const pongMessage: PongMessage = new PongMessage();
---
+++ src/test/java/pingpong/web/controller/MyControllerTest.java
@@ -0,0 +1,25 @@
+package pingpong.web.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class MyControllerTest{
+  @Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testHome() {
+    ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody()).contains("<title>Static");
+	}
+}
